e10e29d9068058643c8b4574ed3fe6a1dc9117d0
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-26 02:10:54 -0400

    Initialize company structure with departments and employees

---
+++ department_salary.js
@@ -0,0 +1,29 @@
+// Task 1: Create a Department Structure
+const company = {
+    departments: [
+    {departmentName: 'Finance',
+    employees: [
+    {name: 'Andy',
+    salary: 1100000,
+    subordinates: [
+    {name: 'AYA',
+    salary: 1150000,
+    subordinates: [
+    {name: 'Anthony',
+    salary: 950000,
+    subordinates: []}]}]},
+    {name: 'Bella',
+    salary: 1000000,
+    subordinates: []}]},
+    {departmentName:'Marketing',
+    employees: [
+    {name: 'Lana',
+    salary: 70000,
+    subordinates: [
+    {name: 'Moana',
+    salary: 65000,
+    subordinates: []}]},
+    {name: 'Hulk',
+    salary: 90000,
+    subordinates: []}]}]};
+    
